63376fc55a16688c82a94a35d50fe2d086d57d9c
Paul Bolle <pebolle@tiscali.nl>
2011-08-09 17:16:28 +0200

    Bluetooth: btusb: be quiet on device disconnect
    Disabling the bluetooth usb device embedded in (some) ThinkPads tends to
    lead to errors like these:
        btusb_bulk_complete: hci0 urb ffff88011b9bfd68 failed to resubmit (19)
        btusb_intr_complete: hci0 urb ffff88011b46a318 failed to resubmit (19)
        btusb_bulk_complete: hci0 urb ffff88011b46a000 failed to resubmit (19)
    
    That is because usb_disconnect() doesn't "quiesces" pending urbs.
    
    Disconnecting a device is a normal thing to happen so it's no big deal
    that usb_submit_urb() returns -ENODEV. The simplest way to get rid of
    these errors is to stop treating that return as an error. Trivial,
    actually.
    
    While we're at it, add comments to be explicit about the reasons we're
    not complaining about -EPERM and -ENODEV.
    
    Signed-off-by: Paul Bolle <pebolle@tiscali.nl>
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>
    (cherry picked from commit 4935f1c164ac528dff3538f97953b385ba500710)

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -272,7 +272,9 @@ static void btusb_intr_complete(struct urb *urb)
 
 	err = usb_submit_urb(urb, GFP_ATOMIC);
 	if (err < 0) {
-		if (err != -EPERM)
+		/* -EPERM: urb is being killed;
+		 * -ENODEV: device got disconnected */
+		if (err != -EPERM && err != -ENODEV)
 			BT_ERR("%s urb %p failed to resubmit (%d)",
 						hdev->name, urb, -err);
 		usb_unanchor_urb(urb);
@@ -357,7 +359,9 @@ static void btusb_bulk_complete(struct urb *urb)
 
 	err = usb_submit_urb(urb, GFP_ATOMIC);
 	if (err < 0) {
-		if (err != -EPERM)
+		/* -EPERM: urb is being killed;
+		 * -ENODEV: device got disconnected */
+		if (err != -EPERM && err != -ENODEV)
 			BT_ERR("%s urb %p failed to resubmit (%d)",
 						hdev->name, urb, -err);
 		usb_unanchor_urb(urb);
@@ -447,7 +451,9 @@ static void btusb_isoc_complete(struct urb *urb)
 
 	err = usb_submit_urb(urb, GFP_ATOMIC);
 	if (err < 0) {
-		if (err != -EPERM)
+		/* -EPERM: urb is being killed;
+		 * -ENODEV: device got disconnected */
+		if (err != -EPERM && err != -ENODEV)
 			BT_ERR("%s urb %p failed to resubmit (%d)",
 						hdev->name, urb, -err);
 		usb_unanchor_urb(urb);
